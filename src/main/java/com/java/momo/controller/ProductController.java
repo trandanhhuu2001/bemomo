3b2f117e85a9d8fe16395acf994106a65e16d4f1
trandanhhuu <76518520+trandanhhuu@users.noreply.github.com>
2024-07-03 16:20:11 +0700

    format code

--- src/main/java/com/java/momo/controller/ProductController.java
+++ src/main/java/com/java/momo/controller/ProductController.java
@@ -13,7 +13,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/product")
 @AllArgsConstructor
-@CrossOrigin(origins = "*")
 public class ProductController {
 
     private final ProductService productService;
